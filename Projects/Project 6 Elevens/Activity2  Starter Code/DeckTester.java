73fcd3245127abd847052e99f3b889efa33be725
797077 <42552641+797077@users.noreply.github.com>
2019-02-25 10:22:36 -0800

    project elevens completed activity 3

--- Projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
+++ Projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
@@ -8,6 +8,30 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    String[] ranks1 = {"a", "b", "c"};
+	    String[] suits1 = {"A", "B", "C"};
+	    int[] values1 = {1, 2};
+	    Deck one = new Deck(ranks1, suits1, values1);
+	    while (!one.isEmpty()){
+	        System.out.println(one.deal().toString());
+	       }
+	    System.out.println("\n");
+	       
+	    String[] ranks2 = {"d", "e", "f"};
+	    String[] suits2 = {"D", "E", "F"};
+	    int[] values2 = {3, 4};
+	    Deck two = new Deck(ranks2, suits2, values2);
+	    while (!two.isEmpty()){
+	        System.out.println(two.deal().toString());
+	       }
+	    System.out.println("\n");
+	    
+	    String[] ranks3 = {"g", "h", "i"};
+	    String[] suits3 = {"G", "H", "I"};
+	    int[] values3 = {5, 6};
+	    Deck three = new Deck(ranks3, suits3, values3);
+	    while (!three.isEmpty()){
+	        System.out.println(three.deal().toString());
+	       }
 	}
 }
--- Projects/Project 6 Elevens/Activity3  Starter Code/Shuffler.java
+++ Projects/Project 6 Elevens/Activity3  Starter Code/Shuffler.java
@@ -50,7 +50,17 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int k = 0;
+		for (int j = 0; j < values.length/2; j++){
+		    shuffled[k] = values[j];
+		    k += 2;
+		  }
+		k = 1;
+		for (int j = values.length/2; j < values.length; j++){
+		    shuffled[k] = values[j];
+		    k += 2;
+		  }
 	}
 
 	/**
@@ -65,6 +75,17 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void selectionShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int maxIndex = 0;
+		for (int k = values.length-1; k >= 1; k--){
+		    for (int r = 0; r <= k; r++){
+		        if (values[r] > values[maxIndex]){
+		            maxIndex = r;
+		          }
+		      }
+		    int temp = values[k];
+		    values[k] = values[maxIndex];
+		    values[maxIndex] = temp;
+		  }
 	}
 }
