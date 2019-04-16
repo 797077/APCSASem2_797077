59fcf9eb389fae6b269fa59cd503488a63146baa
797077 <42552641+797077@users.noreply.github.com>
2019-04-15 17:01:18 -0700

    Update SomeClass.java

--- Labs/Lab 0415/SomeClass.java
+++ Labs/Lab 0415/SomeClass.java
@@ -14,11 +14,11 @@ public class SomeClass
      */
     public SomeClass()
     {
-        
+        stringList = new ArrayList<String>();
     }
 
     /**
-     * sorts strings in alphabetical order using selection sort
+     * a) sorts strings in alphabetical order using selection sort
      */
     public void sortStrings(ArrayList<String> names)
     {
@@ -32,24 +32,35 @@ public class SomeClass
             String smallerNumber = names.get(index);//places that value at the end of the sorted section by swapping
             names.set(index, names.get(i));
             names.set(i, smallerNumber);
-         }
+       }
     }
     
     /**
-     * An example of a method - replace this comment with your own
+     * b) shuffles the strings in the ArrayList parameter
      */
     public void shuffleStrings(ArrayList<String> names)
     {
-       for (int i = 0; i < names.size(); i++){
-           
-        }
+       for (int k = names.size() - 1; k > 0; k--) {
+            int howMany = k + 1;
+            int start = 0;
+            int randPos = (int) (Math.random() * howMany) + start;
+            String temp = names.get(k);
+            names.set(k, names.get(randPos));
+            names.set(randPos, temp);
+       }
     }
     
     /**
-     * An example of a method - replace this comment with your own
+     * c) inserts the String parameter at the correct place in the ArrayList
      */
-    public void insertString(ArrayList<String> word)
+    public void insertString(String word)
     {
-       
+       for (int i = 0; i < stringList.size()-1; i++){//traverses array
+            if (word.compareTo(stringList.get(i))<=0){
+                stringList.add(i, word);
+                return;
+            }
+       }
+       stringList.add(word);
     }
 }
