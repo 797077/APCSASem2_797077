c657d8e4df75e3084fad423a69178f3c30427cbb
797077 <42552641+797077@users.noreply.github.com>
2019-04-15 17:00:50 -0700

    Update GridTester.java

--- Labs/Lab 0415/GridTester.java
+++ Labs/Lab 0415/GridTester.java
@@ -1,33 +1,63 @@
 
 /**
- * Lab 0415 Classwork Question 5
+ * Lab 0415 Classwork Question 5a
  *
  * @author (Grace Jau)
  * @version (0415)
  */
 public class GridTester
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Cell[][] cellGrid;
 
     /**
      * Constructor for objects of class GridTester
      */
-    public GridTester()
+    public GridTester(int rows, int columns)
     {
-        // initialise instance variables
-        x = 0;
+        cellGrid = new Cell[rows][columns];
+        for (int r = 0; r < cellGrid.length; r++){
+            for (int c = 0; c < cellGrid[r].length; c++){
+                cellGrid[r][c] = new Cell();
+            }
+        }
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * randomly sets 30% of the cell's occupied property to true and the rest to false
      */
-    public int sampleMethod(int y)
+    public void loadOccupied()
     {
-        // put your code here
-        return x + y;
+        for (int r = 0; r < cellGrid.length; r++){
+            for (int c = 0; c < cellGrid[r].length; c++){
+                if ((int)(Math.random()*10)<3){
+                    cellGrid[r][c].setOccupied(true);
+                }else{
+                    cellGrid[r][c].setOccupied(false);
+                }
+            }
+        }
+    }
+    
+    /**
+     * will load neighbors with all cells adjacent to the cell that are not occupied
+     */
+    public void loadNeigbors()
+    {
+        for (int r = 0; r < cellGrid.length; r++){
+            for (int c = 0; c < cellGrid[r].length; c++){
+                if (r < cellGrid.length-1 && !cellGrid[r+1][c].getOccupied()){
+                    cellGrid[r][c].getNeighbors().add(cellGrid[r+1][c]);
+                }
+                if (r > 0 && !cellGrid[r-1][c].getOccupied()){
+                    cellGrid[r][c].getNeighbors().add(cellGrid[r-1][c]);
+                }
+                if (c < cellGrid[r].length-1 && !cellGrid[r][c+1].getOccupied()){
+                    cellGrid[r][c].getNeighbors().add(cellGrid[r][c+1]);
+                }
+                if (c > 0 && !cellGrid[r][c-1].getOccupied()){
+                    cellGrid[r][c].getNeighbors().add(cellGrid[r][c-1]);
+                }
+            }
+        }
     }
 }
